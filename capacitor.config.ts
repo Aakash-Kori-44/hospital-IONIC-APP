23041a1469da102e9c258300261b1d51b75fc914
Aakash-kabir-kori <152686993+Aakash-kabir-kori@users.noreply.github.com>
2024-07-30 17:17:38 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MoodleProject',
+  webDir: 'www'
+};
+
+export default config;
