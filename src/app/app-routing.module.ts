64f7a25e5148de9429f93258d1acae3213fc8a38
Aakash-kabir-kori <152686993+Aakash-kabir-kori@users.noreply.github.com>
2024-09-27 12:12:57 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,32 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    redirectTo: 'contact',
+    pathMatch: 'full'
+  },
+  {
+    path: 'contact',
+    loadChildren: () => import('./contact/contact.module').then( m => m.ContactPageModule)
+  },
+  {
+    path: 'create-an-account',
+    loadChildren: () => import('./create-an-account/create-an-account.module').then( m => m.CreateAnAccountPageModule)
+  },
+  
+  {
+    path: 'forgot',
+    loadChildren: () => import('./forgot/forgot.module').then( m => m.ForgotPageModule)
+  }, 
+  {
+    path: 'search-result',
+    loadChildren: () => import('./search-result/search-result.module').then( m => m.SearchResultPageModule)
+  },
+  {
+    path: 'search-details',
+    loadChildren: () => import('./search-details/search-details.module').then( m => m.SearchDetailsPageModule)
   }
-];
+
+ ];
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
